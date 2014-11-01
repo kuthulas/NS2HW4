4d80c61caca6c21610e2a55e1ba9b5e862fcdf81
jnaneshm <jnaneshm@users.noreply.github.com>
2014-10-31 19:54:25 -0500

    Update robot.sh

--- robot.sh
+++ robot.sh
@@ -7,14 +7,15 @@ cases="1 2 3"
 rm -f out_*
 rm -f *.tr
 rm -f *.nam
+rm res.txt 
 
 for a in $flavs
 do
 	for b in $cases
 	do
 		ns hw4.tcl $a $b
-		perl throughput.pl "out_$a$b-S1.tr" 2 0.0 4.0 100 | awk 'NR > 1 { total += $2; count++ } END { print "S1 " total/count }'
-		perl throughput.pl "out_$a$b-S2.tr" 2 1.0 5.0 100 | awk 'NR > 1 { total += $2; count++ } END { print "S2 " total/count }'
+		perl throughput.pl "out_$a$b-S1.tr" 2 0.0 4.0 100 | awk 'NR > 1 { total += $2; count++ } END { print "S1 " total/count }'>>res.txt
+		perl throughput.pl "out_$a$b-S2.tr" 2 1.0 5.0 100 | awk 'NR > 1 { total += $2; count++ } END { print "S2 " total/count }'>>res.txt
 	done
 done
 
